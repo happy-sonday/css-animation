fb3b1bafc4be8a32da733517e64abd3040555729
happy-sonday <mrsmiler1106@gmail.co.kr>
2021-07-04 00:03:15 +0900

    wall 3d scroll ui

--- 3d_scroll_move_ani/3dani.js
+++ 3d_scroll_move_ani/3dani.js
@@ -2,6 +2,8 @@
 (() => {
   const houseElem = document.querySelector(".house");
   const barElem = document.querySelector(".progress-bar");
+  const stageElem = document.querySelector(".stage");
+  const mousePos = { x: 0, y: 0 };
   let maxScrollValue;
 
   /** 사용자 화면 비율에 맞춰 resize */
@@ -13,11 +15,21 @@
   window.addEventListener("scroll", function () {
     const scrollPer = this.pageYOffset / maxScrollValue;
     const zMove = scrollPer * 980 - 490;
+
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
 
     //progress-bar
 
     barElem.style.width = `${scrollPer * 100}%`;
+  });
+
+  //마우스에 이동에 따른 시점 변경
+  window.addEventListener("mousemove", function (e) {
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform = `rotateX(${mousePos.y * 10}deg) rotateY(${
+      mousePos.x * 10
+    }deg)`;
   });
 
   window.addEventListener("resize", resizeHandler);
