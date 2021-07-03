7066db4e40e508d6c2e28baeb59a59cb4bc748fe
happy-sonday <mrsmiler1106@gmail.co.kr>
2021-07-03 23:40:35 +0900

    프로그레스 바

--- 3d_scroll_move_ani/3dani.js
+++ 3d_scroll_move_ani/3dani.js
@@ -1,15 +1,23 @@
 //NOTE:즉시 실행 함수
 (() => {
   const houseElem = document.querySelector(".house");
+  const barElem = document.querySelector(".progress-bar");
   let maxScrollValue;
 
   /** 사용자 화면 비율에 맞춰 resize */
   function resizeHandler() {
     maxScrollValue = document.body.offsetHeight - window.innerHeight;
   }
+
+  console.log(barElem);
   window.addEventListener("scroll", function () {
-    const zMove = (this.pageYOffset / maxScrollValue) * 980 - 490;
+    const scrollPer = this.pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
+
+    //progress-bar
+
+    barElem.style.width = `${scrollPer * 100}%`;
   });
 
   window.addEventListener("resize", resizeHandler);
